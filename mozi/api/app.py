182ed9d6834f6d864f488d168896f492fb5592ce
tonshlee <tonshlee@gmail.com>
2025-03-09 10:00:12 +0000

    Rewrite validation_error_handler

--- mozi/api/app.py
+++ mozi/api/app.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Request
+# pylint: disable=W0613
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse, PlainTextResponse
 
 from mozi.api.errors import APIError
@@ -8,10 +10,22 @@ app = FastAPI(debug=is_debug())
 
 
 @app.exception_handler(APIError)
-async def api_error_handler(request: Request, exc: APIError) -> JSONResponse:  # pylint: disable=W0613
+async def api_error_handler(request: Request, exc: APIError) -> JSONResponse:
     return JSONResponse(content=exc.dict(), status_code=exc.status_code)
 
 
 @app.exception_handler(Exception)
-async def custom_error_handler(request: Request, exc: Exception) -> PlainTextResponse:  # pylint: disable=W0613
+async def custom_error_handler(request: Request, exc: Exception) -> PlainTextResponse:
     return PlainTextResponse(f"Internal Server Error: {str(exc)}", status_code=500)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_error_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+    msg = "Invalid "
+    for error in exc.errors():
+        msg += f" {'.'.join(error['loc'])}: {error['msg']};"
+
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={'error_code': 102, 'detail': msg},
+    )
